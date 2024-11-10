2214bbdeb2bc7347a84b01f464a427e3a39038c0
jimsh7237 <jimsh7237@gmail.com>
2024-11-10 21:35:52 +0100

    adding the game tests

--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -1,30 +1,40 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
-
+from math_quiz import generate_integer, random_operator, create_problem
 
 class TestMathGame(unittest.TestCase):
 
-    def test_function_A(self):
-        # Test if random numbers generated are within the specified range
-        min_val = 1
-        max_val = 10
-        for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
-            self.assertTrue(min_val <= rand_num <= max_val)
+    def test_generate_integer(self):
+        """Test that generate_integer returns a value within the specified range."""
+        min_value, max_value = 1, 10
+        result = generate_integer(min_value, max_value)
+        self.assertGreaterEqual(result, min_value, "Result should be >= min_value")
+        self.assertLessEqual(result, max_value, "Result should be <= max_value")
 
-    def test_function_B(self):
-        # TODO
-        pass
+    def test_random_operator(self):
+        """Test that random_operator returns one of the valid operators."""
+        valid_operators = ['+', '-', '*']
+        result = random_operator()
+        self.assertIn(result, valid_operators, "Result should be one of '+', '-', '*'")
 
-    def test_function_C(self):
-            test_cases = [
-                (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
-            ]
+    def test_create_problem(self):
+        """Test that create_problem returns the correct problem string and answer."""
+        # Test addition
+        num1, num2, operator = 3, 4, '+'
+        problem, answer = create_problem(num1, num2, operator)
+        self.assertEqual(problem, "3 + 4", "Problem string should be formatted correctly")
+        self.assertEqual(answer, 7, "Answer should be 7 for 3 + 4")
 
-            for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
+        # Test subtraction
+        num1, num2, operator = 10, 4, '-'
+        problem, answer = create_problem(num1, num2, operator)
+        self.assertEqual(problem, "10 - 4", "Problem string should be formatted correctly")
+        self.assertEqual(answer, 6, "Answer should be 6 for 10 - 4")
 
-if __name__ == "__main__":
+        # Test multiplication
+        num1, num2, operator = 2, 5, '*'
+        problem, answer = create_problem(num1, num2, operator)
+        self.assertEqual(problem, "2 * 5", "Problem string should be formatted correctly")
+        self.assertEqual(answer, 10, "Answer should be 10 for 2 * 5")
+
+if __name__ == '__main__':
     unittest.main()
